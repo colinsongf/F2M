31981e6dc789a8b5447c13f2cbd06a8de72c4323
aksnzhy <1301111230@pku.edu.cn>
2016-07-19 10:51:01 +0800

    update dense_vector.h

--- src/common/dense_vector.h
+++ src/common/dense_vector.h
@@ -16,3 +16,77 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 */
 // Copyright (c) 2016 by contributors.
 // Author: Chao Ma (mctt90@gmail.com)
+//
+// Define the class template DenseVector and operations required
+// by this class.
+//
+#ifndef F2M_COMMON_DENSE_VECTOR_H_
+#define F2M_COMMON_DENSE_VECTOR_H_
+
+#include "src/common/carray.h"
+#include "src/common/common.h"
+
+namespace f2m {
+
+template <class ValueType>
+class DenseVector : public CArray<ValueType> {
+ public:
+  DenseVector(int size, const ValueType& init)
+    : CArray<ValueType>(size, init) {}
+
+  DenseVector()
+    : CArray() {}
+};
+
+// Scale(v, c) : v <- v * c
+template<class ValueType, class ScaleType>
+void Scale(DenseVector<ValueType>* v,
+           const ScaleType& c) {
+  // TODO: we can use openmp to speedup the performance here.
+  for (int i = 0; i < v->size()l ++i) {
+    (*v)[i] *= c;
+  }
+}
+
+// Scale Into(u,v,c) : u <- v * c
+template <class ValueType, class ScaleType>
+void ScaleInto(DenseVector<ValueType>* u,
+               const DenseVector<ValueType>& v,
+               const ScaleType& c) {
+  CHECK_EQ(v.size(), u->size());
+  CHECK_LT(0, v.size());
+  // TODO: we can use openmp to speedup the performance here.
+  for (int i = 0; i < v.size(); ++i) {
+    (*u)[i] = v[i] * c;
+  }
+}
+
+// AddScaled(u,v,c) : u <- u + v * c
+template <class ValueType, class ScaleType>
+void AddScaled(DenseVector<ValueType>* u,
+               const DenseVector<ValueType>& v,
+               const ScaleType& c) {
+  CHECK_EQ(v.size(), u->size());
+  CHECK_LT(0, v.size());
+  // TODO: we can use openmp to speedup the performance here.
+  for (int i = 0; i < v.size(); ++i) {
+    (*u)[i] += v[i] * c;
+  }
+}
+
+// AddScaledInto(w,u,v,c) : w <- u + v * c
+template <class ValueType, class ScaleType>
+void AddScaledInto(DenseVector<ValueType>* w,
+                   const DenseVector<ValueType>& u,
+                   const DenseVector<ValueType>& v,
+                   const ScaleType& c) {
+  CHECK_EQ(u.size(), v.size());
+  CHECK_EQ(u.size(), w->size());
+  CHECK_LT(0, u.size());
+  // TODO: we can use openmp to speedup the performance here.
+                       
+}
+
+} // namespace f2m
+
+#endif // F2M_COMMON_DENSE_VECTOR_H_
