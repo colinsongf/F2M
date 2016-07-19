506738a316f9d2b5b9db6536c468a87e8f778947
aksnzhy <1301111230@pku.edu.cn>
2016-07-19 21:55:40 +0800

    udpate

--- src/loss/loss.h
+++ src/loss/loss.h
@@ -17,24 +17,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 // Copyright (c) 2016 by contributors.
 // Author: Chao Ma (mctt90@gmail.com)
 //
-// The basic class of a loss function.
-// Loss is an abstract class, which can be implemented by real 
-// loss functions such as logistic regresiion loss (logit_loss.h),
-// FM loss (fm_loss.h), and FFM loss (ffm_loss.h).
+// The basic class of a loss function. Loss is an abstract class, 
+// which can be implemented by real loss functions such as logistic 
+// regresiion loss (logit_loss.h), FM loss (fm_loss.h), and FFM 
+// loss (ffm_loss.h).
 //
-// There are 3 important functions in a loss class:
-/* 
- *  1. virtual void Predict(const StringList* row_data,
- *                          const CArrayList* model_param,
- *                          FloatList* pred_results) = 0;
- *
- *  2. virtual void CalcGrad(const StringList* row_data,
- *                           const CArrayList* model_param,
- *                           FloatList* grad) = 0;
- *
- *  3. virtual float Evaluate(const FloatList* pred_results,
- *                            const FloatList* labels);
- */ 
 // User can implement different Predict, CalcGrad, and Evaluate functions 
 // in their derived classes by using different algorithms. 
 //
@@ -43,42 +30,37 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #ifndef F2M_LOSS_LOSS_H_
 #define F2M_LOSS_LOSS_H_
 
-#include <vector>
-#include <string>
-#include <map>
-#include <cmath>
+#include <cmath>  // for log() and exp()
 
-#include "src/common/carray.h"
 #include "src/common/common.h"
+#include "src/common/data_ml.h"  // define the type of Model and ValueList
+#include "src/common/dense_vector.h"
+#include "src/common/sparse_vector.h"
 
 namespace f2m {
 
-typedef std::vector<std::string> StringList;     // To store the training and testing data.
-typedef CArray<float> FloatList;                 // float is sufficient enough for ML problems.
-typedef map< CArray<float> > CArrayList;         // To handle the model of LR, FM, as well as FFM.
+typedef std::vector<std::string> StringList;
 
 class Loss {
  public:
   virtual ~Loss() {}
 
-  // Given the testing data records and current model, 
-  // return the prediction result. Note that the prediction result 
-  // is represented as a float number in both classification and
-  // regression problems.
-  virtual void Predict(const StringList* row_data,
-                       const CArrayList* model_param,
-                       FloatList* pred_results) = 0;
+  // Given the testing data records and current model, return the prediction 
+  // result. Note that the prediction result is represented as a real number 
+  // in (defined in src/common/data_ml.h) both classification and regression problems.
+  virtual void Predict(const StringList* row_data, 
+                       const Model* model_param,
+                       ValueList* pred) = 0;
 
-  // Given the traning data records and current model, 
-  // return the gradients.
+  // Given the traning data records and current model, return the gradients.
   virtual void CalcGrad(const StringList* row_data,
-                        const CArrayList* model_param,
-                        FloatList* grad) = 0;
+                        const Model* model_param,
+                        ValueList* grad) = 0;
 
-  // Given the prediction results and the true labels,
-  // return current loss value. Here we use LogLoss in default.
-  virtual float Evaluate(const FloatList* pred_results,
-                         const FloatList* labels) const {
+  // Given the prediction results and the true labels, return current loss value. 
+  // Here we use LogLoss in default.
+  virtual float Evaluate(const ValueList* pred_results,
+                         const ValueList* labels) const {
     float objv = 0.0;
     for (size_t i = 0; i < pred_results.size(); ++i) {
       float y = label[i] > 0 ? 1 : -1;
@@ -86,10 +68,6 @@ class Loss {
     }
     return objv;
   }
-
- protected:
-  Parser* parser_;   // We need a parser here because the input format of the data
-                     // is different in LR, FM, and FFM.
 
  private:
   DISALLOW_COPY_AND_ASSIGN(Loss);
