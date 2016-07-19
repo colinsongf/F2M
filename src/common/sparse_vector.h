39eb50e11194981eee733c6cfc4efaf1aa3cbedc
aksnzhy <1301111230@pku.edu.cn>
2016-07-19 13:42:59 +0800

    update sparse_vector.h

--- src/common/sparse_vector.h
+++ src/common/sparse_vector.h
@@ -86,7 +86,106 @@ bool has(const KeyType& key) const {
 template <class KeyType, class ValueType>
 const ValueType SparseVector<KeyType, ValueType>::zero_(0);
 
+// Scale(v,c) : v <- v * c
+template <class KeyType, class ValueType, class ScaleType>
+void Scale(SparseVector<KeyType, ValueType>* v,
+           const ScaleType& c) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  for (typename SV::iterator i = v->begin(); i != v->end(); ++i) {
+    i->second *= c;
+  }
+}
 
+// ScaleInto(u,v,c) : u <- v * c
+template <class KeyType, class ValueType, class ScaleType>
+void ScaleInto(SparseVector<KeyType, ValueType>* u,
+               const SparseVector<KeyType, ValueType>& v,
+               const ScaleType& c) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  u->clear();
+  for (typename SV::const_iterator i = v.begin(); i != v.end(); ++i) {
+    u->set(i->first, i->second * c);
+  }
+}
+
+// AddScaled(u,v,c) : u <- u + v * c
+template <class KeyType, class ValueType, class ScaleType>
+void AddScaled(SparseVector<KeyType, ValueType>*u,
+               const SparseVector<KeyType, ValueType>& v,
+               const ScaleType& c) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  for (typename SV::const_iterator i = v.begin(); i != v.end(); ++i) {
+    u->set(i->first, (*u)[i->first] + i->second * c);
+  }
+}
+
+// AddScaledInto(w,u,v,c) : w <- u + v * c
+template <class KeyType, class ValueType, class ScaleType>
+void AddScaledInto(SparseVector<KeyType, ValueType>* w,
+                   const SparseVector<KeyType, ValueType>& u,
+                   const SparseVector<KeyType, ValueType>& v,
+                   const ScaleType& c) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  w->clear();
+  typename SV::const_iterator i = u.begin();
+  typename SV::const_iterator j = v.begin();
+  while (i != u.end() && j != v.end()) {
+    if (i->first == j->first) {
+      w->set(i->first, i->second + j->second * c);
+      ++i;
+      ++j;
+    } else if (i->first < j->first) {
+      w->set(i->first, i->second);
+      ++i;
+    } else {
+      w->set(j->first, j->second * c);
+      ++j;
+    }
+  }
+  while (i != u.end()) {
+    w->set(i->first, i->second);
+    ++i;
+  }
+  while (j != v.end()) {
+    w->set(j->first, j->second * c);
+    ++j;
+  }
+}
+
+// DotProduct(u,v) : r <- dot(u, v);
+template <class KeyType, class ValueType>
+ValueType DotProduct(const SparseVector<KeyType, ValueType>& v1,
+                     const SparseVector<KeyType, ValueType>& v2) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  typename SV::const_iterator i = v1.begin();
+  typename SV::const_iterator j = v2.begin();
+  ValueType ret = 0;
+  while (i != v1.end() && j != v2.end()) {
+    if (i->first == j->first) {
+      ret += i->second * j->second;
+      ++i;
+      ++j;
+    } else if (i->first < j->first) {
+      ++i;
+    } else {
+      ++j;
+    }
+  }
+  return ret;
+}
+
+// Output a sparse vector in human readable format.
+template <class KeyType, class ValueType>
+ostream& operator<<(ostream& output,
+                    const SparseVector<KeyType, ValueType>& vec) {
+  typedef SparseVector<KeyType, ValueType> SV;
+  output << "[ ";
+  for (typename SV::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+    output << i->first << ":" << i->second << " ";
+  }
+  output << "]";
+  return output;
+}
 
 } // namespace f2m
 
