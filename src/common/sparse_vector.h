e36178ee0259471bcb074138c3c357d70191432a
aksnzhy <1301111230@pku.edu.cn>
2016-07-19 12:23:27 +0800

    update sparse_vector.h

--- src/common/sparse_vector.h
+++ src/common/sparse_vector.h
@@ -16,3 +16,78 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 */
 // Copyright (c) 2016 by contributors.
 // Author: Chao Ma (mctt90@gmail.com)
+//
+// Define the class template SparseVector and operations required
+// by this class.
+//
+#ifndef F2M_COMMON_SPARSE_VECTOR_H_
+#define F2M_COMMON_SPARSE_VECTOR_H_
+
+#include <map>
+
+namespace f2m {
+
+using std::map;
+using std::pair;
+using std::ostream;
+
+// We use std::map as the container of a sparse vector. Because
+// std::map implements a sorting tree (RB-tree), so its iterator
+// acesses elements in known order of keys. This is important for 
+// sparse vector operations like dot-product and add-mutil-into.
+// The ValueType must be a numeric type supporting const 0.
+template <class KeyType, class ValueType>
+class SparseVector : public map<KeyType, ValueType> {
+ public:
+  typedef typename map<KeyType, ValueType>::const_iterator const_iterator;
+  typedef typename map<KeyType, ValueType>::iterator iterator;
+  
+  // We constrain operator[] a read-only operation to prevent
+  // accidential insert of elements.
+  const ValueType& operator[](const KeyType& key) const {
+    const_iterator iter = this->find(key);
+    if (iter == this->end()) {
+      return zero_; 
+    }
+    return iter->second;
+  }
+
+// Set a value at given key. If value == 0, an existing key-value 
+// pair is removed. If value != 0, the value is set or inserted.
+// This function also serves as a convenient form of insert(), no
+// need to use std::pair.
+void set(const KeyType& key, const ValueType& value) {
+  iterator iter = this->find(key);
+  if (iter != this->end()) {
+    if (IsZero(value)) {
+      this->erase(iter);
+    } else {
+      iter->second = value;
+    }
+  } else {
+    if (!IsZero(value)) {
+      this->insert(pair<KeyType, ValueType>(key, value));
+    }
+  }
+}
+
+bool has(const KeyType& key) const {
+  return this->find(key) != this->end();
+}
+
+ protected:
+  static const ValueType zero_;
+
+  static bool IsZero(const ValueType& value) {
+    return value == 0;    
+  }
+};
+
+template <class KeyType, class ValueType>
+const ValueType SparseVector<KeyType, ValueType>::zero_(0);
+
+
+
+} // namespace f2m
+
+#endif // F2M_COMMON_SPARSE_VECTOR_H_
