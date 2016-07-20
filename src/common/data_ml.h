ac78b1a3a7eb28d325f4ea6baa94041852f15669
aksnzhy <1301111230@pku.edu.cn>
2016-07-20 12:48:42 +0800

    update data_ml.h

--- src/common/data_ml.h
+++ src/common/data_ml.h
@@ -16,3 +16,37 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 */
 // Copyright (c) 2016 by contributors.
 // Author: Chao Ma (mctt90@gmail.com)
+//
+// Define common data structures and data types used in F2M.
+//
+#ifndef F2M_COMMON_DATA_ML_H_
+#define F2M_COMMON_DATA_ML_H_
+
+#include "src/common/common.h"
+#include "src/common/sparse_vector.h"
+#include "src/common/dense_vector.h"
+
+namespace f2m {
+
+// This define the float point that will be used 
+// to store feature value and gradient.
+typedef float real_t;
+
+// This defines the unsigned integer type that
+// can normally be used to store feature index
+typedef uint32 index_t;
+
+// Define sparse value list, which is used to be store feature
+// value list and gradient list.
+typedef SparseVector<index_t, real_t> SparseList;
+
+// Define dense value list.
+typedef DenseVector<real_t> DenseList;
+
+// Define the model used in LR, FM, as well as in FFM.
+
+
+
+} // namespace f2m
+
+#endif // F2M_COMMON_DATA_ML_H_
