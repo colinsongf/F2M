87706f478cec28104ef0aebcf3f6ddd5437f77e7
aksnzhy <1301111230@pku.edu.cn>
2016-07-25 13:13:23 +0800

    update dense_vector.h

--- src/common/dense_vector.h
+++ src/common/dense_vector.h
@@ -14,20 +14,29 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
 */
-// Copyright (c) 2016 by contributors.
-// Author: Chao Ma (mctt90@gmail.com)
-//
-// Define the class template DenseVector and operations required
-// by this class.
-//
+
+/* 
+Copyright (c) 2016 by contributors.
+Author: Chao Ma (mctt90@gmail.com)
+
+Define the class template DenseVector and the operations 
+required by this class.
+*/
+
 #ifndef F2M_COMMON_DENSE_VECTOR_H_
 #define F2M_COMMON_DENSE_VECTOR_H_
 
-#include "src/common/common.h"
+#include "src/common/common.h" // use CArray class
 
-namespace f2m {
+/* -----------------------------------------------------------------------------
+ * DenseVector inherit the CArray class, which is designed to encapsulate 
+ * C-style vectors with a minimum interface. Compared with std::vector, 
+ * CArray ensures that elements are stored successively in memory layout, 
+ * and provides an interface to access the elements in C-style.
+ * -----------------------------------------------------------------------------
+ */
 
-template <class ValueType>
+template <typename ValueType>
 class DenseVector : public CArray<ValueType> {
  public:
   DenseVector(int size, const ValueType& init)
@@ -37,43 +46,54 @@ class DenseVector : public CArray<ValueType> {
     : CArray<ValueType>(size) {}
 };
 
-// Scale(v, c) : v <- v * c
-template<class ValueType, class ScaleType>
-void Scale(DenseVector<ValueType>* v,
-           const ScaleType& c) {
-  // TODO: we can use openmp to speedup the performance here.
+/* -----------------------------------------------------------------------------
+ * This file also provides a set of mathmatic operations used by a DenseVector.
+ * -----------------------------------------------------------------------------
+ */
+
+/* Scale(v, c) : v <- v * c 
+ */
+template<typename ValueType, typename ScaleType>
+void Scale(DenseVector<ValueType>* v, const ScaleType& c) {
+  // TODO: we can use openmp to speedup 
+  // the performance here.
   for (int i = 0; i < v->size(); ++i) {
     ((*v).data())[i] *= c;
   }
 }
 
-// Scale Into(u,v,c) : u <- v * c
-template <class ValueType, class ScaleType>
-void ScaleInto(DenseVector<ValueType>* u,
+/* Scale Into(u,v,c) : u <- v * c 
+ */
+template <typename ValueType, typename ScaleType>
+void ScaleInto(DenseVector<ValueType>* u, 
                const DenseVector<ValueType>& v,
                const ScaleType& c) {
   CHECK_EQ(v.size(), u->size());
   CHECK_LT(0, v.size());
-  // TODO: we can use openmp to speedup the performance here.
+  // TODO: we can use openmp to speedup 
+  // the performance here.
   for (int i = 0; i < v.size(); ++i) {
     ((*u).data())[i] = v.data()[i] * c;
   }
 }
 
-// AddScaled(u,v,c) : u <- u + v * c
+/* AddScaled(u,v,c) : u <- u + v * c
+ */
 template <class ValueType, class ScaleType>
 void AddScaled(DenseVector<ValueType>* u,
                const DenseVector<ValueType>& v,
                const ScaleType& c) {
   CHECK_EQ(v.size(), u->size());
   CHECK_LT(0, v.size());
-  // TODO: we can use openmp to speedup the performance here.
+  // TODO: we can use openmp to speedup 
+  // the performance here.
   for (int i = 0; i < v.size(); ++i) {
     ((*u).data())[i] += v.data()[i] * c;
   }
 }
 
-// AddScaledInto(w,u,v,c) : w <- u + v * c
+/* AddScaledInto(w,u,v,c) : w <- u + v * c
+ */
 template <class ValueType, class ScaleType>
 void AddScaledInto(DenseVector<ValueType>* w,
                    const DenseVector<ValueType>& u,
@@ -82,26 +102,30 @@ void AddScaledInto(DenseVector<ValueType>* w,
   CHECK_EQ(u.size(), v.size());
   CHECK_EQ(u.size(), w->size());
   CHECK_LT(0, u.size());
-  // TODO: we can use openmp to speedup the performance here.
+  // TODO: we can use openmp to speedup 
+  // the performance here.
   for (int i = 0; i < u.size(); ++i) {
     ((*w).data())[i] = u.data()[i] + v.data()[i] * c;
   }
 }
 
-// DotProduct(u,v) : r <- dot(u, v)
+/* DotProduct(u,v) : r <- dot(u, v)
+ */
 template <class ValueType>
 ValueType DotProduct(const DenseVector<ValueType>& v1,
                      const DenseVector<ValueType>& v2) {
   CHECK_EQ(v1.size(), v2.size());
   ValueType ret = 0;
-  // TODO: we can use openmp to speedup the performance here.
+  // TODO: we can use openmp to speedup 
+  // the performance here.
   for (int i = 0; i < v1.size(); ++i) {
     ret += v1.data()[i] * v2.data()[i];
   }
   return ret;
 }
 
-// Output a sparse vector in human readable format.
+/* Output a dense vector in human readable format.
+ */
 template <class ValueType>
 std::ostream& operator<< (std::ostream& output,
                           const DenseVector<ValueType>& vec) {
@@ -109,11 +133,10 @@ std::ostream& operator<< (std::ostream& output,
   for (int i = 0; i < vec.size(); ++i) {
     if (vec.data()[i] != 0) // to keep the format the same with sparse
       output << i << ":" << vec.data()[i] << " ";
+    if (i % 100 == 0) output << endl;
   }
   output << "]";
   return output;
 }
-
-} // namespace f2m
 
 #endif // F2M_COMMON_DENSE_VECTOR_H_
