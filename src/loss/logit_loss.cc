1ebbcf8660119caaf8ae7202219e0bafddd60eb1
aksnzhy <1301111230@pku.edu.cn>
2016-07-28 10:35:07 +0800

    update logit_loss.h

--- src/loss/logit_loss.cc
+++ src/loss/logit_loss.cc
@@ -14,5 +14,51 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
 */
-// Copyright (c) 2016 by contributors.
-// Author: Chao Ma (mctt90@gmail.com)
+
+/* 
+Copyright (c) 2016 by contributors.
+Author: Chao Ma (mctt90@gmail.com)
+
+This file implements the logit_loss.h
+*/
+
+#include "src/loss/logit_loss.h"
+
+#include "src/common/common.h"
+#include "src/common/data_structure.h"
+#include "src/common/linear_algebra.h"
+
+/* ---------------------------------------------------------------------------
+ * Given the input data matrix and current model, return the prediction 
+ * results. Note that the prediction result is represented as a real 
+ * number (float point type) in both classification and 
+ * regression problems. Math: 
+ *   
+ *  [ pred += X * w ] 
+ *
+ * where 'pred' is the prediction vector, X is the input data matrix, 
+ * w is current model parameter represented as a vector.
+ * ---------------------------------------------------------------------------
+ */
+void LogitLoss::Predict(const DMatrix& data_matrix,
+                        const DSVector& model_param,
+                        DSVector* pred) {
+  
+}
+
+/* ---------------------------------------------------------------------------
+ * Given the input data matrix and current model, return 
+ * the calculated gradients. Math: 
+ *
+ *  [ pred = X * w ]
+ *  [ p = -y / (1 + exp(y * pred)) ]
+ *  [ grad += X' * p ]
+ *
+ * where, 
+ * ---------------------------------------------------------------------------
+ */
+void LogitLoss::CalcGrad(const DMatrix* row_data,
+                         const DSVector* model_param,
+                         DSVector* grad) {
+	
+}
--- src/loss/logit_loss.h
+++ src/loss/logit_loss.h
@@ -14,42 +14,65 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 02110-1301, USA.
 */
-// Copyright (c) 2016 by contributors.
-// Author: Chao Ma (mctt90@gmail.com)
-//
-// Loss class for Logistic Regression.
-//
-// For Logistic Regression, we use LogLoss for classification problems.
-//
+
+/*
+Copyright (c) 2016 by contributors.
+Author: Chao Ma (mctt90@gmail.com)
+
+This file defines the logistic regression loss.
+*/
+
 #ifndef F2M_LOSS_LOGIT_LOSS_H_
 #define F2M_LOSS_LOGIT_LOSS_H_
 
-#include <vector>
-#include <string>
-
-#include "src/common/carray.h"
 #include "src/common/common.h"
+#include "src/common/data_structure.h"
 
 #include "src/loss/loss.h"
 
 namespace f2m {
 
+/* -----------------------------------------------------------------------------
+ * Logistic Regression Loss:
+ * 
+ *  math: l(x, y, w) = log(1 + exp(-y * <w, x>))
+ *
+ * -----------------------------------------------------------------------------
+ */
 class LogitLoss : public Loss {
  public:
-  LogitLoss();
-  ~Logistic();
+  ~Logistic() {}
 
-  // Given the testing data records, return the prediction results.
-  // Note that, for LR, the model_param has only one element, i.e.,
-  // model_param[0] is the real parameter vector.
-  void Predict(const StringList* row_data,
-               const CArrayList* model_param,
-               FloatList* pred_results);
+  /* ---------------------------------------------------------------------------
+   * Given the input data matrix and current model, return the prediction 
+   * results. Note that the prediction result is represented as a real 
+   * number (float point type) in both classification and 
+   * regression problems. Math: 
+   *   
+   *  [ pred = X * w ] 
+   *
+   * where 'pred' is the prediction vector, X is the input data matrix, 
+   * w is current model parameter represented as a vector.
+   * ---------------------------------------------------------------------------
+   */
+  void Predict(const DMatrix& data_matrix,
+               const DSVector& model_param,
+               DSVector* pred);
 
-  // Given the training data records, return the gradients.
-  void CalcGrad(const StringList* row_data,
-                const CArrayList* model_param,
-                FloatList* grad);
+  /* ---------------------------------------------------------------------------
+   * Given the input data matrix and current model, return 
+   * the calculated gradients. Math: 
+   *
+   *  [ pred = X * w ]
+   *  [ p = -y / (1 + exp(y * pred)) ]
+   *  [ grad += X' * p ]
+   *
+   * where, 
+   * ---------------------------------------------------------------------------
+   */
+  void CalcGrad(const DMatrix* row_data,
+                const DSVector* model_param,
+                DSVector* grad) = 0;
 
  private: 
   DISALLOW_COPY_AND_ASSIGN(LogitLoss);
