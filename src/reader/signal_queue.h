13c799a9397dd21f520883b4fa0b65ba387b444f
aksnzhy <1301111230@pku.edu.cn>
2016-08-14 21:49:23 +0800

    update

--- src/reader/signal_queue.h
+++ src/reader/signal_queue.h
@@ -1,10 +1,24 @@
-/*************************************************************************
-	> File Name: signal_queue.h
-	> Author: 
-	> Mail: 
-	> Created Time: 日  8/14 21:10:42 2016
- ************************************************************************/
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at
+your option) any later version.
 
-#ifndef _SIGNAL_QUEUE_H
-#define _SIGNAL_QUEUE_H
-#endif
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA.
+*/
+
+/* 
+Copyright (c) 2016 by contributors.
+Author: Chao Ma (mctt90@gmail.com)
+
+This files defines Parser class, which parse the Reader's 
+output to a DMatrix.
+*/
